c4f0ccd9398bdede24e654f3c3dd4e6890247b27
REZA-OJAGHI-DRO <frontenddro@gmail.com>
2024-03-28 04:36:14 +0330

    reza

--- src/app/page3/page.jsx
+++ src/app/page3/page.jsx
@@ -14,7 +14,7 @@ export default function Page3() {
   }
 
   return (
-    <main className="w-[100%] h-[100vh] sm:h-[100vh] xl:h-[100vh] bg-slate-900">
+    <main className="w-[100%] h-[100vh] sm:h-[100vh] xl:h-[100vh] bgTheme">
       <BackGround />
       <Header />
       <MenuList />
@@ -52,13 +52,13 @@ export default function Page3() {
 function BackGround() {
   return (
     <div className="w-[100%] h-[100%] absolute top-0 left-0 overflow-hidden">
-      <div className="w-[100%] h-[100vh] sm:h-[100vh] lg:h-[100vh] bg-[rgb(9,9,17)] bgStake">
-        <article className="w-[100%] h-[100%] overflow-hidden flex justify-center pt-[20vh]">
+      <div className="w-[100%] h-[80vh] sm:h-[80vh] lg:h-[80vh] bg-[rgb(9,9,17)] bgStake">
+        <article className="w-[100%] h-[100%]  flex justify-center pt-[20vh]">
           {/* <div className="colorBackground  w-[60%] h-[20vh]  opacity-30 blur-[80px] "></div> */}
         </article>
       </div>
 
-      <section className="w-[100%] h-[20vh] sm:h-[20vh] xl:h-[20vh] bg-[#0a0a10]">
+      <section className="w-[100%] h-[40vh] sm:h-[40vh] xl:h-[40vh] bg-[#0a0a10]">
         <div className="w-[100%] h-[100%] flex justify-center  backdropBlur">
           <div className="w-[35vw] h-[35vw] rounded-full translate-y-[0vh] colorBackground2 opacity-70 -rotate-[70deg] blur-[270px]"></div>
         </div>
